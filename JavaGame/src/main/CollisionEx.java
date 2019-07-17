447814981a324bf8afec090d212195f8a688b940
XiqianZ <makeherreal@gmail.com>
2019-07-17 11:09:08 +0900

    update main

---
+++ JavaGame/src/main/CollisionEx.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import shape.Board;
+
+public class CollisionEx extends JFrame {
+
+    public CollisionEx() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Collision");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            CollisionEx ex = new CollisionEx();
+            ex.setVisible(true);
+        });
+    }
+}
